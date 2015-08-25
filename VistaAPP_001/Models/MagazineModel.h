4203a818ce1061d9fc574bf0dff46789f050acc9
YeJinliang <liudz_nepu@126.com>
2015-08-25 17:23:39 +0800

    1

---
+++ VistaAPP_001/Models/MagazineModel.h
@@ -0,0 +1,21 @@
+//
+//  MagazineModel.h
+//  VistaAPP_001
+//
+//  Created by qianfeng on 15/8/25.
+//  Copyright (c) 2015年 柳德智. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MagazineModel : NSObject
+
+@property (nonatomic, copy) NSString * magazineID;
+@property (nonatomic, copy) NSString * desc;
+@property (nonatomic, copy) NSString * cover;
+@property (nonatomic, copy) NSString * pub_time;
+@property (nonatomic, copy) NSString * title;
+@property (nonatomic, copy) NSString * year;
+@property (nonatomic, copy) NSString * vol_year;
+
+@end
---
+++ VistaAPP_001/Models/MagazineModel.m
@@ -0,0 +1,16 @@
+//
+//  MagazineModel.m
+//  VistaAPP_001
+//
+//  Created by qianfeng on 15/8/25.
+//  Copyright (c) 2015年 柳德智. All rights reserved.
+//
+
+#import "MagazineModel.h"
+
+@implementation MagazineModel
+
+-(void)setValue:(id)value forUndefinedKey:(NSString *)key{
+}
+
+@end
