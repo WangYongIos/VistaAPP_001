9a2f2c2e0355b133bd686e3778cc6a4a787b26a7
YeJinliang <liudz_nepu@126.com>
2015-08-25 14:08:28 +0800

    Initial Commit

---
+++ VistaAPP_001/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VistaAPP_001
+//
+//  Created by qianfeng on 15/8/25.
+//  Copyright (c) 2015年 柳德智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VistaAPP_001Tests/VistaAPP_001Tests.m
@@ -0,0 +1,34 @@
+//
+//  VistaAPP_001Tests.m
+//  VistaAPP_001Tests
+//
+//  Created by qianfeng on 15/8/25.
+//  Copyright (c) 2015年 柳德智. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VistaAPP_001Tests : XCTestCase
+
+@end
+
+@implementation VistaAPP_001Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
